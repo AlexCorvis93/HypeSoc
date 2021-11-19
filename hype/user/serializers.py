d17b5152911886dc97500c84edb09adec2bbf241
Aleksandr <zuboff_a@mail.ru>
2021-11-19 18:50:34 +0500

    add image for following users.

--- hype/user/serializers.py
+++ hype/user/serializers.py
@@ -13,9 +13,9 @@ class ProfileSerializer(serializers.ModelSerializer):
 
 class PostSerializer(serializers.ModelSerializer):
     author = serializers.PrimaryKeyRelatedField(read_only=True)
-    title = serializers.CharField(max_length=400,  style={'placeholder': 'Title', 'autofocus': True, 'input_type': 'title'})
-    text = serializers.CharField(max_length=1000, style={'base_template': 'textarea.html', 'rows': 4, 'placeholder': 'Text'})
-    category = serializers.ChoiceField(choices=Category_choices, style={'base_template': 'select.html'})
+    # title = serializers.CharField(max_length=400,  style={'template': "form_html/form_post.html"})
+    # text = serializers.CharField(max_length=1000, style={'template': "form_html/text.html"})
+    # category = serializers.ChoiceField(choices=Category_choices)
     img = serializers.ImageField(max_length=None, allow_empty_file=False, use_url=True)
 
     class Meta:
