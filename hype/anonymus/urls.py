ef15fe78e628f6a497566240bb5b22dfb9d8c7b6
Aleksandr <zuboff_a@mail.ru>
2022-01-17 20:37:28 +0500

    include script

--- hype/anonymus/urls.py
+++ hype/anonymus/urls.py
@@ -3,6 +3,6 @@ from .views import NewsList, detail_and_comments
 
 urlpatterns = [
    path('', NewsList.as_view(), name='a_news'),# news page
-   path('post/<int:pk>', detail_and_comments, name='post')
+   path('post/<int:pk>', detail_and_comments, name='post')# post_page
 
 ]
